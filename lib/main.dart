af8d10eabcef175d2c333d0813777898e9e67357
sergiogarciaseco@usal.es <ser356@ser356.ser356>
2024-06-21 01:45:03 +0200

    First Usable Version
    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,21 @@
-import 'package:device_apps/device_apps.dart';
 import 'package:flutter/material.dart';
+import 'package:no_phone/aux/page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp(key: Key('myAppKey')));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: FutureBuilder<List<Application>>(
-          future: DeviceApps.getInstalledApplications(
-            includeSystemApps: true,
-            onlyAppsWithLaunchIntent: true,
-          ),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data?.length ?? 0,
-                itemBuilder: (context, index) {
-                  Application app = snapshot.data![index];
-                  return ListTile(
-                    title: Text(
-                      app.appName,
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: () => DeviceApps.openApp(app.packageName),
-                  );
-                },
-              );
-            }
-          },
-        ),
+      theme: ThemeData(
+        brightness: Brightness.dark,
       ),
+      home: const MyPage(),
     );
   }
 }
+
